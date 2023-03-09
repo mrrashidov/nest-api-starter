ed98532da06b5482437cdcd6bf03a37dc574be90
Rashidov Shoxrux <mrrashidov@hotmail.com>
2023-03-09 16:16:28 +0500

    fix(shared): added mail service

---
+++ src/shared/services/mail.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@nestjs/common';
+import { createTransport } from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class MailService {
+  constructor(private readonly configService: ConfigService) {}
+
+  /**
+   *
+   * @param to - list of receivers
+   * @param action - Action line
+   * @example
+   *       to: 'bar@example.com, baz@example.com',
+   *       action: 'email-verification',
+   */
+  async send({ to, action }: { to: string; action: string }) {
+    const transporter = await createTransport({
+      host: this.configService.get('MAIL_HOST') || 'smtp.kibera.uz',
+      port: this.configService.get('MAIL_PORT') || 587,
+      secure: this.configService.get('MAIL_ENCRYPTION') || false, // true for 465, false for other ports
+      auth: {
+        user: this.configService.get('MAIL_USERNAME'),
+        pass: this.configService.get('MAIL_PASSWORD'),
+      },
+    });
+    return transporter.sendMail(this.templates(action, { to }));
+  }
+
+  /**
+   * @param action - string
+   * @param to - string
+   * @private
+   */
+  private templates(action: string, { to }: { to: string }) {
+    switch (action) {
+      case 'user-created':
+        return {
+          from: `${this.configService.get(
+            'MAIL_FROM_NAME',
+          )} <${this.configService.get('MAIL_FROM_ADDRESS')}>`,
+          to,
+          subject: 'Kibera account',
+          html: '<b>Email verification message here</b>',
+        };
+      case 'email-verification':
+        return {
+          from: `${this.configService.get(
+            'MAIL_FROM_NAME',
+          )} <${this.configService.get('MAIL_FROM_ADDRESS')}>`,
+          to,
+          subject: 'Kibera account',
+          html: '<b>Email verification message here</b>',
+        };
+      case 'forgot-password':
+        return {
+          from: `${this.configService.get(
+            'MAIL_FROM_NAME',
+          )} <${this.configService.get('MAIL_FROM_ADDRESS')}>`,
+          to,
+          subject: 'Kibera account security info',
+          html: '<b>Forgot password message here</b>',
+        };
+      case 'reset-password':
+        return {
+          from: `${this.configService.get(
+            'MAIL_FROM_NAME',
+          )} <${this.configService.get('MAIL_FROM_ADDRESS')}>`,
+          to,
+          subject: 'Kibera account security info',
+          html: '<b>Reset password message here</b>',
+        };
+      case 'welcome':
+        return {
+          from: `${this.configService.get(
+            'MAIL_FROM_NAME',
+          )} <${this.configService.get('MAIL_FROM_ADDRESS')}>`,
+          to,
+          subject: 'Kibera account',
+          html: '<b>Welcome message here</b>',
+        };
+    }
+  }
+}
