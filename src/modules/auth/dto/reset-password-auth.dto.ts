165b6748cf51c387e8262678412374071f7cad86
Rashidov Shoxrux <mrrashidov@hotmail.com>
2023-03-10 16:28:21 +0500

    fix(auth): added reset-password validation dto

--- src/modules/auth/dto/reset-password-auth.dto.ts
+++ src/modules/auth/dto/reset-password-auth.dto.ts
@@ -1,5 +1,17 @@
+import { Match } from '@/shared/decorators/match.decorator';
+import { IsNotEmpty, IsString, Length, NotContains } from 'class-validator';
+
 export class ResetPasswordAuthDto {
+  @IsNotEmpty()
+  @NotContains(' ')
+  @Length(6, 100)
   token: string;
+
+  @IsString()
+  @Length(6, 100)
   password: string;
+
+  @IsNotEmpty()
+  @Match('password_confirmation')
   password_confirmation: string;
 }
