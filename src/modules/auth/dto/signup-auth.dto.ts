d25fe07142a5b66e848e51965c6154cdbcb6c0ea
Rashidov Shoxrux <mrrashidov@hotmail.com>
2023-03-10 16:26:03 +0500

    fix(auth): added register validation

--- src/modules/auth/dto/signup-auth.dto.ts
+++ src/modules/auth/dto/signup-auth.dto.ts
@@ -1,7 +1,33 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  NotContains,
+} from 'class-validator';
+
 export class SignUpAuthDto {
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsPhoneNumber()
   phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 25)
   first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 25)
   last_name: string;
+
+  @IsNotEmpty()
+  @NotContains(' ')
+  @Length(6, 100)
   password: string;
 }
