5f59288e823fd8a3863f71ef3e50fe009fac3ad1
Rashidov Shoxrux <mrrashidov@hotmail.com>
2023-03-10 16:28:35 +0500

    fix(auth): added forgot-password validation dto

--- src/modules/auth/dto/forgot-password-auth.dto.ts
+++ src/modules/auth/dto/forgot-password-auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsOptional, IsPhoneNumber } from 'class-validator';
+
 export class ForgotPasswordAuthDto {
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsPhoneNumber()
   phone?: string;
 }
