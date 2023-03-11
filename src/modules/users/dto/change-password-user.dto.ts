2f4c3c9de704e0fc55a8e00e2e30ca0f1b46eb51
Rashidov Shoxrux <mrrashidov@hotmail.com>
2023-03-11 16:57:58 +0500

    fix(users): added change password validation dto

--- src/modules/users/dto/change-password-user.dto.ts
+++ src/modules/users/dto/change-password-user.dto.ts
@@ -1,4 +1,12 @@
+import { Match } from '@/shared/decorators/match.decorator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class ChangePasswordUserDto {
+  @IsString()
+  @Length(6, 100)
   password: string;
+
+  @IsNotEmpty()
+  @Match('password_confirmation')
   password_confirmation: string;
 }
